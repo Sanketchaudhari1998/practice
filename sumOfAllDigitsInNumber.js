bd580891d7f7a3079b1fb493444de1c82748548b
Sanketchaudhari1998 <csanket18@gmail.com>
2023-08-25 18:36:13 +0530

    sum of all digits in given number after formatted by prettier
    sum of all digits in given number after formatted by prettier

--- sumOfAllDigitsInNumber.js
+++ sumOfAllDigitsInNumber.js
@@ -1,13 +1,12 @@
-function number(num){
-    let sum=0
-let strNum = num.toString()
-for (const iterator of strNum) {
-    let convertedStrNum = parseInt (iterator)
+function number(num) {
+  let sum = 0;
+  let strNum = num.toString();
+  for (const iterator of strNum) {
+    let convertedStrNum = parseInt(iterator);
     sum = sum + convertedStrNum;
+  }
+  console.log(`Sum of all digits in number ${num} is : ${sum}`);
 }
-    console.log(`Sum of all digits in number ${num} is : ${sum}`);
-
-}
-number(123)
-number(456)
-number(6464)
+number(123);
+number(456);
+number(6464);
